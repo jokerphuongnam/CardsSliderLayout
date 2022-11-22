d6ddd0ef8cbac2ef38f83170e6a97c7ddbfcbbf2
P.Nam <nampp@inspirelab.io>
2022-11-22 10:29:42 +0700

    set up base infite scroll collectionview

--- Sources/CardsSliderLayout/InfiniteScrollCollectionView+CardsSlider.swift
+++ Sources/CardsSliderLayout/InfiniteScrollCollectionView+CardsSlider.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  InfiniteScrollCollectionView+CardsSlider.swift
+//  CardsSliderLayout
 //
 //  Created by P.Nam on 22/11/2022.
 //
@@ -9,11 +9,19 @@ import UIKit
 import InfiniteScrollCollectionView
 
 extension InfiniteScrollCollectionView {
-    open override func scrollToItem(at indexPath: IndexPath, at scrollPosition: UICollectionView.ScrollPosition, animated: Bool) {
+    open override var infiniteIndexPathsForVisibleItems: [IndexPath] {
+        if let layout = collectionViewLayout as? CardsSliderLayout {
+            return [[0, layout.selectedItem]]
+        } else {
+            return super.indexPathsForVisibleItems
+        }
+    }
+    
+    open override func infiniteScrollToItem(at indexPath: IndexPath, at scrollPosition: UICollectionView.ScrollPosition) {
         if let layout = collectionViewLayout as? CardsSliderLayout {
             layout.selectedItem = indexPath.item
         } else {
-            super.scrollToItem(at: indexPath, at: scrollPosition, animated: animated)
+            super.scrollToItem(at: indexPath, at: scrollPosition, animated: false)
         }
     }
 }
